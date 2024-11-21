0935ee8b6dc3d41079f75f8488496b9aa832d2c2
Zachary Cheng <zach.ch02@gmail.com>
2024-11-21 13:18:58 +0800

    refactor: remove TODO comment for using string builder in Repeat function

--- go_with_tests/iteration/iteration.go
+++ go_with_tests/iteration/iteration.go
@@ -3,7 +3,6 @@ package iteration
 import "strings"
 
 // Constructs a string that repeats 'character' repeatCount times
-// TODO: Use a string builder to reduce memory allocation
 func Repeat(character string, repeatCount int) string {
 	return strings.Repeat(character, repeatCount)
 }
