268b99611fbfccf5ef089aaf01910f160f2802c3
Zachary Cheng <zach.ch02@gmail.com>
2024-11-21 11:07:03 +0800

    test: enhance unit tests for Repeat function with variable repeats and add benchmark

--- go_with_tests/iteration/iteration_test.go
+++ go_with_tests/iteration/iteration_test.go
@@ -4,11 +4,25 @@ import "testing"
 
 func TestIteration(t *testing.T) {
 	t.Run("Testing standard repeat", func(t *testing.T) {
-		repeated := Repeat("a")
+		repeated := Repeat("a", 0)
 		expected := "aaaaa"
 
 		if repeated != expected {
 			t.Errorf("expected %q but got %q", expected, repeated)
 		}
 	})
+	t.Run("Testing variable amount of repeats", func(t *testing.T) {
+		repeated := Repeat("abc",4)
+		expected := "abcabcabcabc"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q",expected, repeated)
+		}
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
 }
