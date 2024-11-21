328dd7fc1bf5d864b86eea3c0cfc307f00b5a79f
Zachary Cheng <zach.ch02@gmail.com>
2024-11-21 11:09:31 +0800

    docs: add comment to clarify Repeat function's purpose and parameters

--- go_with_tests/iteration/iteration.go
+++ go_with_tests/iteration/iteration.go
@@ -1,5 +1,6 @@
 package iteration
 
+// Constructs a string that repeats 'character' repeatCount times
 func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
