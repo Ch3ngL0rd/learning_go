5e1cb109771c658ec31655baae1d6c0b9ff5dda1
Zachary Cheng <zach.ch02@gmail.com>
2024-11-21 16:27:00 +0800

    refactor: simplify test assertions by introducing assertCorrect function

--- go_with_tests/structs/struct_test.go
+++ go_with_tests/structs/struct_test.go
@@ -5,17 +5,17 @@ import "testing"
 func TestPerimeter(t *testing.T) {
 	got := Perimeter(10.0, 10.0)
 	want := 40.0
-
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
-	}
+	assertCorrect(t, got, want)
 }
 
 func TestArea(t *testing.T) {
 	got := Area(10.0, 10.0)
 	want := 100.0
+	assertCorrect(t, got, want)
+}
 
+func assertCorrect(t testing.TB, got, want float64) {
 	if got != want {
 		t.Errorf("got %.2f, want %.2f", got, want)
-	}
+	}	
 }
