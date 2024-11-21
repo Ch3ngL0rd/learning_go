de85aa087503f1ba86f7a1af0b0d58bcde248635
Zachary Cheng <zach.ch02@gmail.com>
2024-11-21 16:34:55 +0800

    feat: add tests for Circle struct perimeter and area calculations

--- go_with_tests/structs/struct_test.go
+++ go_with_tests/structs/struct_test.go
@@ -1,19 +1,38 @@
 package structs
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
-	assertCorrect(t, got, want)
+	t.Run("Test rectangle perimeter", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Perimeter(rectangle)
+		want := 40.0
+		assertCorrect(t, got, want)
+	})
+	t.Run("Test circle perimeter", func(t *testing.T) {
+		circle := Circle{1.00}
+		got := Perimeter(circle)
+		want := 2 * math.Pi
+		assertCorrect(t, got, want)
+	})
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 100.0
-	assertCorrect(t, got, want)
+	t.Run("Test rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Area(rectangle)
+		want := 100.0
+		assertCorrect(t, got, want)
+	})
+	t.Run("Test circle area", func(t *testing.T) {
+		circle := Circle{1.00}
+		got := Area(circle)
+		want := math.Pi
+		assertCorrect(t, got, want)
+	})
 }
 
 func assertCorrect(t testing.TB, got, want float64) {
