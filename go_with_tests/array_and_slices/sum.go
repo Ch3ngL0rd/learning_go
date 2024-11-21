1646e4cd072fe377761d6794f128d30f53253c7e
Zachary Cheng <zach.ch02@gmail.com>
2024-11-21 15:36:15 +0800

    feat: optimize SumAll function to use indexed assignment for improved performance

--- go_with_tests/array_and_slices/sum.go
+++ go_with_tests/array_and_slices/sum.go
@@ -10,9 +10,9 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	var sums []int
-	for _, numbers := range numbersToSum {
-		sums = append(sums, Sum(numbers))
+	sums := make([]int, len(numbersToSum))
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
 	}
 	return sums
 }
