056b59591801b20587da2720fd529fbd84b10e0b
Zachary Cheng <zach.ch02@gmail.com>
2024-11-21 13:18:45 +0800

    refactor: simplify Repeat function using strings.Repeat for improved readability

--- go_with_tests/iteration/iteration.go
+++ go_with_tests/iteration/iteration.go
@@ -5,9 +5,5 @@ import "strings"
 // Constructs a string that repeats 'character' repeatCount times
 // TODO: Use a string builder to reduce memory allocation
 func Repeat(character string, repeatCount int) string {
-	var sb strings.Builder
-	for i := 0; i < repeatCount; i++ {
-		sb.WriteString(character)
-	}
-	return sb.String()
+	return strings.Repeat(character, repeatCount)
 }
