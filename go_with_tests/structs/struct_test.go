77cc52c4758b96817bbd3957528368194cd04480
Zachary Cheng <zach.ch02@gmail.com>
2024-11-21 16:49:08 +0800

    feat: add Shape interface and refactor tests to use it for Area calculations

--- go_with_tests/structs/struct_test.go
+++ go_with_tests/structs/struct_test.go
@@ -6,33 +6,32 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	t.Run("Test rectangle perimeter", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Perimeter()
-		want := 40.0
-		assertCorrect(t, got, want)
-	})
-	t.Run("Test circle perimeter", func(t *testing.T) {
-		circle := Circle{1.00}
-		got := circle.Perimeter()
-		want := 2 * math.Pi
-		assertCorrect(t, got, want)
-	})
+	perimeterTests := []struct {
+		shape Shape
+		want float64
+	}{
+		{Rectangle{10.0, 10.0}, 40.0},
+		{Circle{1.00}, 2 * math.Pi},
+	}
+	for _, tt := range perimeterTests {
+		got := tt.shape.Area()
+		assertCorrect(t, got, tt.want)
+	}
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Test rectangle area", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		want := 100.0
-		assertCorrect(t, got, want)
-	})
-	t.Run("Test circle area", func(t *testing.T) {
-		circle := Circle{1.00}
-		got := circle.Area()
-		want := math.Pi
-		assertCorrect(t, got, want)
-	})
+	areaTests := []struct {
+		shape Shape
+		want float64
+	}{
+		{Rectangle{12,6}, 72},
+		{Circle{1.00}, math.Pi},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		assertCorrect(t, got, tt.want)
+	}
 }
 
 func assertCorrect(t testing.TB, got, want float64) {
