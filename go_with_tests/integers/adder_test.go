4fa04bba51880ebd2410e78303720b98a8252d4b
Zachary Cheng <zach.ch02@gmail.com>
2024-11-21 10:49:11 +0800

    test: add example and improve tests for Add function

--- go_with_tests/integers/adder_test.go
+++ go_with_tests/integers/adder_test.go
@@ -1,13 +1,31 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
-func TestAdder(t *testing.T) {
+func ExampleAdd() {
+	sum := Add(9, 10)
+	fmt.Println(sum)
+	// Output: 19
+}
+
+func TestAdd(t *testing.T) {
 	t.Run("Testing standard add", func(t *testing.T) {
 		sum := Add(4, 5)
 		expected := 9
-		if sum != expected {
-			t.Errorf("expected %d but got %d", expected, sum)
-		}
+		assertCorrectSum(t, expected, sum)
 	})
+	t.Run("Adding two negative numbers", func(t *testing.T) {
+		sum := Add(-4, -9)
+		expected := -13
+		assertCorrectSum(t, expected, sum)
+	})
+}
+
+func assertCorrectSum(t testing.TB, expected, sum int) {
+	if sum != expected {
+		t.Errorf("expected %d but got %d", expected, sum)
+	}
 }
