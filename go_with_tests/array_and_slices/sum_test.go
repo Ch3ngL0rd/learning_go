65b371ac7d1d8cf74291615398a84966afecb617
Zachary Cheng <zach.ch02@gmail.com>
2024-11-21 15:42:33 +0800

    test: enhance TestSumAllTails with additional test cases for standard and empty inputs

--- go_with_tests/array_and_slices/sum_test.go
+++ go_with_tests/array_and_slices/sum_test.go
@@ -29,10 +29,20 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	got := SumAllTails([]int{1,2}, []int{9,9})
-	want := []int{2, 9}
+	t.Run("Testing standard case", func (t *testing.T) {
+		got := SumAllTails([]int{1,2}, []int{9,9})
+		want := []int{2, 9}
+	
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected %v got %v", want, got)
+		}
+	})
+	t.Run("Testing empty case", func (t *testing.T) {
+		got := SumAllTails([]int{}, []int{3,4,5})
+		want := []int{0, 9}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("expected %v got %v", want, got)
-	}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected %v got %v", want, got)
+		}
+	})
 }
