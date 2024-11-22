37f5b281a3d0286f2306f831ca74600336014fec
Zachary Cheng <zach.ch02@gmail.com>
2024-11-22 14:26:47 +0800

    feat: implement MyGreeterHandler for HTTP requests and update main function to start server

--- go_with_tests/depenedncy/injection.go
+++ go_with_tests/depenedncy/injection.go
@@ -1,8 +1,10 @@
-package depenedncy
+package main
 
 import (
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -10,6 +12,11 @@ func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+	Greet(os.Stdout, " a user has logged on\n")
+}
+
 func main() {
-	Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
