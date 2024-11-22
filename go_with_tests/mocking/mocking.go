0fa41738a3cb336d76c29902a41735ef32633d40
Zachary Cheng <zach.ch02@gmail.com>
2024-11-22 14:58:24 +0800

    feat: implement DefaultSleeper and SpyCountdownOperations for enhanced Countdown functionality

--- go_with_tests/mocking/mocking.go
+++ go_with_tests/mocking/mocking.go
@@ -14,14 +14,41 @@ type SpySleeper struct {
 	Calls int
 }
 
+
 func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
-		fmt.Fprintf(out, "%v\n", i)
-		time.Sleep(1 * time.Second)
-	}
-	fmt.Fprintf(out, "Go!")
+type DefaultSleeper struct{}
+
+func (ds *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
 }
+
+const sleep = "sleep"
+const write = "write"
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const countdownStart = 3
+const finalWord = "Go!"
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
+}
+
--- go_with_tests/mocking/mocking_test.go
+++ go_with_tests/mocking/mocking_test.go
@@ -3,20 +3,45 @@ package mocking
 import (
 	"bytes"
 	"testing"
+	"reflect"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := bytes.Buffer{}
-	spySleeper := SpySleeper{}
-	Countdown(&buffer, &spySleeper)
+	t.Run("Test countdown", func (t *testing.T) {
+		buffer := bytes.Buffer{}
+		spySleeper := SpySleeper{}
+		Countdown(&buffer, &spySleeper)
 
-	got := buffer.String()
-	want := `3
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("expected %q, got %q", want, got)
-	}
+		if got != want {
+			t.Errorf("expected %q, got %q", want, got)
+		}
+
+		if spySleeper.Calls != 3 {
+			t.Errorf("expected %q sleep calls, got %q", 3, spySleeper.Calls)
+		}
+	})
+	t.Run("Test order of countdown", func (t *testing.T) {
+		spySleepPrinter := SpyCountdownOperations{}
+		Countdown(&spySleepPrinter, &spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,	
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v, got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
