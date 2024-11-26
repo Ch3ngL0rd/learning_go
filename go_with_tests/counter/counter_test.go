fb358f70391fc5b1fb3f723b43f6d6c991fa4b31
Zachary Cheng <zach.ch02@gmail.com>
2024-11-26 17:28:14 +0800

    test: add unit test for Counter to verify increment functionality

---
+++ go_with_tests/counter/counter_test.go
@@ -0,0 +1,14 @@
+package counter
+
+import "testing"
+
+func TestCounter(t *testing.T) {
+	counter := Counter{}
+	counter.Inc()
+	counter.Inc()
+	counter.Inc()
+
+	if counter.Value() != 3 {
+		t.Errorf("got %d, wanted %d", counter.Value(), 3)
+	}
+}
