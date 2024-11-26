a32bfce6ce17d800ce4d41e75658b8db159e7720
Zachary Cheng <zach.ch02@gmail.com>
2024-11-26 17:31:12 +0800

    test: enhance TestCounter with subtests for increment scenarios

--- go_with_tests/counter/counter_test.go
+++ go_with_tests/counter/counter_test.go
@@ -3,12 +3,21 @@ package counter
 import "testing"
 
 func TestCounter(t *testing.T) {
-	counter := Counter{}
-	counter.Inc()
-	counter.Inc()
-	counter.Inc()
+	t.Run("0 increments", func (t *testing.T) {
+		counter := Counter{}
 
-	if counter.Value() != 3 {
-		t.Errorf("got %d, wanted %d", counter.Value(), 3)
-	}
+		if counter.Value() != 0 {
+			t.Errorf("got %d, wanted %d", counter.Value(), 0)
+		}
+	})
+	t.Run("3 increments", func (t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+	
+		if counter.Value() != 3 {
+			t.Errorf("got %d, wanted %d", counter.Value(), 3)
+		}
+	})
 }
