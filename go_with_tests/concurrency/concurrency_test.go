669871000fc411b6ce13d06ea2525114a00fbeb8
Zachary Cheng <zach.ch02@gmail.com>
2024-11-26 09:28:30 +0800

    feat: implement CheckWebsites function for concurrent website checking with tests

--- go_with_tests/concurrency/concurrency_test.go
+++ go_with_tests/concurrency/concurrency_test.go
@@ -2,8 +2,33 @@ package concurrency
 
 import (
 	"testing"
+	"reflect"
 	"time"
 )
+
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://furhurterwe.geds"
+}
+
+func TestCheckWebsite(t *testing.T) {
+	websites := []string {
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool {
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,	
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
 
 func slowStubWebsiteChecker(_ string) bool {
 	time.Sleep(20 * time.Millisecond)
