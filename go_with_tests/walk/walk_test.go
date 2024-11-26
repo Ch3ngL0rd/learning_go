cc42bcb7ada7e77adf564b8b3b28e6cfca664b94
Zachary Cheng <zach.ch02@gmail.com>
2024-11-26 17:08:42 +0800

    feat: enhance walk function to support nested structs and multiple fields

--- go_with_tests/walk/walk_test.go
+++ go_with_tests/walk/walk_test.go
@@ -1,23 +1,71 @@
 package walk
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
-
-	x := struct {
-		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Fatalf("wrong number of function calls, got %d want %d", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with a single field",
+			struct {
+				Name string
+			}{"Zac"},
+			[]string{"Zac"},
+		},
+		{
+			"struct with two fields",
+			struct {
+				First string
+				Last  string
+			}{"Zachary", "Cheng"},
+			[]string{"Zachary", "Cheng"},
+		},
+		{
+			"struct with mixed types",
+			struct {
+				First string
+				Age int
+			}{"Zachary", 21},
+			[]string{"Zachary"},
+		},
+		{
+			"empty struct",
+			struct{}{},
+			[]string{},
+		},
+		{
+			"recursive struct",
+			struct{
+				Person struct{
+					Name string
+				}
+				Street string
+			}{
+				struct{
+					Name string
+				}{"Zachary"},
+				"Hi",
+			},
+			[]string{"Zachary", "Hi"},
+		},
 	}
-	if got[0] != expected {
-		t.Errorf("expected value %v, got %v", expected, got[0])
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			got := []string{}
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, expected %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
