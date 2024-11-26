a9dd252a7228a84404bcb1d50971b18dec968b54
Zachary Cheng <zach.ch02@gmail.com>
2024-11-26 14:52:05 +0800

    feat: implement concurrent URL response time comparison in Racer function

--- go_with_tests/racer/racer_test.go
+++ go_with_tests/racer/racer_test.go
@@ -8,21 +8,35 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0)
-
-	defer slowServer.Close()
-	defer fastServer.Close()
+	t.Run("compare speed of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0)
 	
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		defer slowServer.Close()
+		defer fastServer.Close()
+		
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+	
+		want := fastURL
+		got := Racer(slowURL, fastURL)
+	
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("timeout if a server does not respond within 10 seconds", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		defer serverA.Close()
+		defer serverB.Close()
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		_, err := Racer(serverA.URL, serverB.URL)
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
