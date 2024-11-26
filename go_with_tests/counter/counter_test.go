22c414b129c39d174512b1cc2390a5b2d8cb9a78
Zachary Cheng <zach.ch02@gmail.com>
2024-11-26 17:39:36 +0800

    test: add concurrent increments test for Counter

--- go_with_tests/counter/counter_test.go
+++ go_with_tests/counter/counter_test.go
@@ -1,6 +1,9 @@
 package counter
 
-import "testing"
+import (
+	"testing"
+	"sync"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("0 increments", func (t *testing.T) {
@@ -15,6 +18,23 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 	
 		assertValue(t, counter, 3)
+	})
+	t.Run("Test concurrent increments", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertValue(t, counter, wantedCount)
 	})
 }
 
